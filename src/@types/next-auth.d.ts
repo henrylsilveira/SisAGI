c48f28dd6be4e6d3328b485ea8b73397288dd88b
Henry Leão <henrylsilveira@gmail.com>
2023-01-09 15:09:58 -0400

    09/01/2023

--- src/@types/next-auth.d.ts
+++ src/@types/next-auth.d.ts
@@ -6,6 +6,7 @@ declare module "next-auth" {
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
+    id?: string;
     militar: Militar,
     user: {
       /** The user's postal address. */
