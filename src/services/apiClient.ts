d6641d83cc27b3a0a678568b643fc199ee703ca3
Henry Leão <henrylsilveira@gmail.com>
2023-08-02 14:15:09 -0400

    02/08/2023

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export function setupAPIClient() {
 
   const api = axios.create({
-    baseURL: 'https://sisagiserver.onrender.com:3333'
+    baseURL: 'https://sisagiserver.onrender.com'
       // process.env.NODE_ENV === "development"
       //   ? "http://localhost:3333/"
       //   : "http://localhost:10000/",
