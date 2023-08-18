ab6eecf2e9812b1ee0af52403daa87192e0f9b2b
Henry Leão <henrylsilveira@gmail.com>
2023-08-17 20:59:44 -0400

    17/08/2023

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -3,11 +3,11 @@ import axios from 'axios';
 export function setupAPIClient() {
 
   const api = axios.create({
-    baseURL: 
+    baseURL: 'https://sisagiserver.onrender.com/'
     // 'https://b0cd-18-228-7-33.ngrok.io'
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:3333/"
-        : 'https://b0cd-18-228-7-33.ngrok.io',
+      // process.env.NODE_ENV === "development"
+      //   ? "http://localhost:3333/"
+      //   : 'https://sisagiserver.onrender.com/',
     // headers: {
     //     Authorization: `Bearer ${cookies['nextauth.token']}`, 
     // }
