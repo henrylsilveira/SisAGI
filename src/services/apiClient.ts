302ee56dbd21e6c8ff05e4e1a9457354ac256271
Henry Leao <henrylsilveira@gmail.com>
2024-05-22 23:26:12 -0300

    Corrigido o endereço da API

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -3,9 +3,10 @@ import axios from 'axios';
 export function setupAPIClient() {
 
   const api = axios.create({
+    //Colocar endereço com barra no final para nao quebrar a rota dos avatares
     baseURL: process.env.NODE_ENV === "development"
-        ? "http://localhost:3333"
-        : 'http://10.35.88.12:3333',
+        ? "http://localhost:3333/"
+        : 'http://10.35.88.12:3333/',
     // headers: {
     //     Authorization: `Bearer ${cookies['nextauth.token']}`, 
     // }
